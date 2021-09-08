38c93cb3fa8e674573f03712c550e182a68c03b0
unknown <matthewharre@gmail.com>
2021-09-08 04:31:05 -0700

    added 3.4 exercises

---
+++ Chapter-3/3.4-Anagrams.py
@@ -0,0 +1,48 @@
+
+def anagramSolution1(s1,s2):
+    stillOK = True
+    if len(s1) != len(s2):
+        stillOK = False
+
+    alist = list(s2)
+    pos1 = 0
+
+    while pos1 < len(s1) and stillOK:
+        pos2 = 0
+        found = False
+        while pos2 < len(alist) and not found:
+            if s1[pos1] == alist[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+
+        if found:
+            alist[pos2] = None
+        else:
+            stillOK = False
+
+        pos1 = pos1 + 1
+
+    return stillOK
+
+print(anagramSolution1('abcd','dcba'))
+
+def anagramSolution2(s1,s2):
+    alist1 = list(s1)
+    alist2 = list(s2)
+
+    alist1.sort()
+    alist2.sort()
+
+    pos = 0
+    matches = True
+
+    while pos < len(s1) and matches:
+        if alist1[pos]==alist2[pos]:
+            pos = pos + 1
+        else:
+            matches = False
+
+    return matches
+
+print(anagramSolution2('abcde','edcba'))
