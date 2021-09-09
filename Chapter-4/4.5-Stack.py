0326ae4a268df476c8fb61983e0cc54f7b6531e2
unknown <matthewharre@gmail.com>
2021-09-09 03:47:19 -0700

    stack demonstration with lists

---
+++ Chapter-4/4.5-Stack.py
@@ -0,0 +1,39 @@
+from pythonds.basic import Stack
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def __repr__(self):
+        return str(self.items)
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+s = Stack()
+print(s.is_empty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.is_empty())
+s.push(8.4)
+print(s)
+print(s.pop())
+print(s.pop())
+print(s.size())
+print(s)
