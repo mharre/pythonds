7017a4264cf7df075a555995546b20ce605eb93a
unknown <matthewharre@gmail.com>
2021-09-15 08:46:13 -0700

    added randranges to different parts in the tree with a helper func

--- Chapter-5/5.7-RecursionVisualized.py
+++ Chapter-5/5.7-RecursionVisualized.py
@@ -1,27 +1,46 @@
 import turtle
+from random import randrange
 
-# myTurtle = turtle.Turtle()
-# myWin = turtle.Screen()
+myTurtle = turtle.Turtle()
+myWin = turtle.Screen()
 
-# def drawSpiral(myTurtle, lineLen):
-#     if lineLen > 0:
-#         myTurtle.forward(lineLen)
-#         myTurtle.right(90)
-#         drawSpiral(myTurtle,lineLen-5)
+def drawSpiral(myTurtle, lineLen):
+    if lineLen > 0:
+        myTurtle.forward(lineLen)
+        myTurtle.right(90)
+        drawSpiral(myTurtle,lineLen-5)
 
-# drawSpiral(myTurtle,100)
-# myWin.exitonclick()
+drawSpiral(myTurtle,100)
+myWin.exitonclick()
+
+def help(n):
+    n = randrange(1,15)
+    return n
 
 def tree(branchLen,t):
     if branchLen > 5:
+        a = randrange(1,15)
         t.forward(branchLen)
         t.right(20)
         # t.pensize(abs(t.pensize()-1))
-        tree(branchLen-15,t)
+        tree(branchLen-help(a),t)
         t.left(40)
-        tree(branchLen-15,t)
+        tree(branchLen-help(a),t)
         t.right(20)
         t.backward(branchLen)
+
+
+# def tree(branchLen,t):
+#     if branchLen > 5:
+#         t.forward(branchLen)
+#         t.right(20)
+#         # t.pensize(abs(t.pensize() - 1)) 
+#         tree(branchLen-15,t)
+#         t.left(40)
+#         tree(branchLen-15,t)
+#         t.right(20)
+#         t.backward(branchLen)
+
 
 
 def main():
@@ -35,5 +54,6 @@ def main():
     t.pensize(5)
     tree(75,t)
     myWin.exitonclick()
+
 
 main()
