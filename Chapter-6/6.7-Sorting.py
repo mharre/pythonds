a1e4d349e0552470aeef901b80a6d2f940b53cce
unknown <matthewharre@gmail.com>
2021-09-20 08:28:12 -0700

    added selection sort exercise

--- Chapter-6/6.7-Sorting.py
+++ Chapter-6/6.7-Sorting.py
@@ -7,8 +7,22 @@ def bubbleSort(alist):
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
 
-alist = [54,26,93,17,77,31,44,55,20]
-alist1 = [54,26,93,17]
-bubbleSort(alist1)
-print(alist1)
+# alist = [54,26,93,17,77,31,44,55,20]
+# alist1 = [54,26,93,17]
+# bubbleSort(alist1)
+# print(alist1)
 
+def selectionSort(alist):
+   for fillslot in range(len(alist)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alist[location]>alist[positionOfMax]:
+               positionOfMax = location
+
+       temp = alist[fillslot]
+       alist[fillslot] = alist[positionOfMax]
+       alist[positionOfMax] = temp
+
+alist = [54,26,93,17,77,31,44,55,20]
+selectionSort(alist)
+print(alist)
