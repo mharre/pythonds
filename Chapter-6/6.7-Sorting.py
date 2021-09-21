a47cf00e494202a0134c1e5c631290c370dbc2b7
unknown <matthewharre@gmail.com>
2021-09-21 03:26:20 -0700

    added more insertion examples

--- Chapter-6/6.7-Sorting.py
+++ Chapter-6/6.7-Sorting.py
@@ -23,6 +23,50 @@ def selectionSort(alist):
        alist[fillslot] = alist[positionOfMax]
        alist[positionOfMax] = temp
 
+# alist = [54,26,93,17,77,31,44,55,20]
+# selectionSort(alist)
+# print(alist)
+
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+
+# alist = [54,26,93,17,77,31,44,55,20]
+# insertionSort(alist)
+# print(alist)
+
+def shellSort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+
+      for startposition in range(sublistcount):
+        gapInsertionSort(alist,startposition,sublistcount)
+
+      print("After increments of size",sublistcount,
+                                   "The list is",alist)
+
+      sublistcount = sublistcount // 2
+
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+
+        currentvalue = alist[i]
+        position = i
+
+        while position>=gap and alist[position-gap]>currentvalue:
+            alist[position]=alist[position-gap]
+            position = position-gap
+
+        alist[position]=currentvalue
+
 alist = [54,26,93,17,77,31,44,55,20]
-selectionSort(alist)
+shellSort(alist)
 print(alist)
