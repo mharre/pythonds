b6cfc26e94ec2fb044f38baab0301e753ee81775
unknown <matthewharre@gmail.com>
2021-09-22 05:53:52 -0700

    added some more node exercises and parsetree exercises

---
+++ Chapter-7/7.6-ParseTree.py
@@ -0,0 +1,89 @@
+from pythonds.basic import Stack
+from pythonds.trees import BinaryTree
+import operator
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+
+    def preorder(self):
+        print(self.key)
+        if self.leftChild:
+            self.leftChild.preorder()
+        if self.rightChild:
+            self.rightChild.preorder()
+
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+
+        elif i == ')':
+            currentTree = pStack.pop()
+
+        elif i not in ['+', '-', '*', '/', ')']:
+            try:
+                currentTree.setRootVal(int(i))
+                parent = pStack.pop()
+                currentTree = parent
+
+            except ValueError:
+                raise ValueError("token '{}' is not a valid integer".format(i))
+
+    return eTree
+
+
+def evaluate(parseTree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC),evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+def postordereval(tree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+    res1 = None
+    res2 = None
+    if tree:
+        res1 = postordereval(tree.getLeftChild())
+        res2 = postordereval(tree.getRightChild())
+        if res1 and res2:
+            return opers[tree.getRootVal()](res1,res2)
+        else:
+            return tree.getRootVal()
+
+def printexp(tree):
+  sVal = ""
+  if tree:
+      sVal = '(' + printexp(tree.getLeftChild())
+      sVal = sVal + str(tree.getRootVal())
+      sVal = sVal + printexp(tree.getRightChild())+')'
+  return sVal
+
+def inorder(tree):
+  if tree != None:
+      inorder(tree.getLeftChild())
+      print(tree.getRootVal())
+      inorder(tree.getRightChild())
+
+
+pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+# pt.postorder()  
+# evaluate(pt)
+# print(pt.preorder()) #to check preorder traversal
+print(inorder(pt)) #to check inorder traversal
