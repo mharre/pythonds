93411151a8e3f2ae0fd58ca9c09de69bdd238d5c
unknown <matthewharre@gmail.com>
2021-09-22 02:04:56 -0700

    added small tree representation as list example

---
+++ Chapter-7/ListofLists.py
@@ -0,0 +1,47 @@
+myTree = ['a',   #root
+      ['b',  #left subtree
+       ['d', [], []],
+       ['e', [], []] ],
+      ['c',  #right subtree
+       ['f', [], []],
+       [] ]
+     ]
+
+def binaryTree(r):
+    return [r, [], []]
+
+def insertLeft(root,newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]])
+    else:
+        root.insert(1,[newBranch, [], []])
+    return root
+
+def insertRight(root,newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newBranch,[],t])
+    else:
+        root.insert(2,[newBranch,[],[]])
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root,newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+r = binaryTree('a')
+insertLeft(r,'b')
+insertLeft(getLeftChild(r), 'd')
+insertRight(r,'c')
+insertRight(getRightChild(r), 'e')
+insertRight(getRightChild(getRightChild(r)), 'e')
+print(r)
