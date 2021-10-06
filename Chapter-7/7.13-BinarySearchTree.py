669d4f2580413c2754cf45f73abb986e78aa3b8c
unknown <matthewharre@gmail.com>
2021-10-06 09:28:35 -0700

    added more logic to AVLtree, fixed indention issues in both binary search tree and avl tree

--- Chapter-7/7.13-BinarySearchTree.py
+++ Chapter-7/7.13-BinarySearchTree.py
@@ -163,39 +163,39 @@ class BinarySearchTree:
        self.delete(key)
 
     def remove(self,currentNode):
-         if currentNode.isLeaf(): #leaf
-           if currentNode == currentNode.parent.leftChild:
+        if currentNode.isLeaf(): #leaf
+            if currentNode == currentNode.parent.leftChild:
                currentNode.parent.leftChild = None
-           else:
+            else:
                currentNode.parent.rightChild = None
-         elif currentNode.hasBothChildren(): #interior
-           succ = currentNode.findSuccessor()
-           succ.spliceOut()
-           currentNode.key = succ.key
-           currentNode.payload = succ.payload
+        elif currentNode.hasBothChildren(): #interior
+            succ = currentNode.findSuccessor()
+            succ.spliceOut()
+            currentNode.key = succ.key
+            currentNode.payload = succ.payload
 
-         else: # this node has one child
-           if currentNode.hasLeftChild():
-             if currentNode.isLeftChild():
-                 currentNode.leftChild.parent = currentNode.parent
-                 currentNode.parent.leftChild = currentNode.leftChild
-             elif currentNode.isRightChild():
-                 currentNode.leftChild.parent = currentNode.parent
-                 currentNode.parent.rightChild = currentNode.leftChild
-             else:
-                 currentNode.replaceNodeData(currentNode.leftChild.key,
+        else: # this node has one child
+            if currentNode.hasLeftChild():
+                if currentNode.isLeftChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.leftChild
+                elif currentNode.isRightChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.leftChild
+                else:
+                    currentNode.replaceNodeData(currentNode.leftChild.key,
                                     currentNode.leftChild.payload,
                                     currentNode.leftChild.leftChild,
                                     currentNode.leftChild.rightChild)
-           else:
-             if currentNode.isLeftChild():
-                 currentNode.rightChild.parent = currentNode.parent
-                 currentNode.parent.leftChild = currentNode.rightChild
-             elif currentNode.isRightChild():
-                 currentNode.rightChild.parent = currentNode.parent
-                 currentNode.parent.rightChild = currentNode.rightChild
-             else:
-                 currentNode.replaceNodeData(currentNode.rightChild.key,
+            else:
+                if currentNode.isLeftChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.rightChild
+                elif currentNode.isRightChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.rightChild
+                else:
+                    currentNode.replaceNodeData(currentNode.rightChild.key,
                                     currentNode.rightChild.payload,
                                     currentNode.rightChild.leftChild,
                                     currentNode.rightChild.rightChild)
