2797e3c23f0bdb8e07d3f97adfa3daca5f66fff3
unknown <matthewharre@gmail.com>
2021-10-18 04:25:32 -0700

    added traffic light state example

--- RandomExercises/turtles.py
+++ RandomExercises/turtles.py
@@ -30,18 +30,53 @@ import turtle
 
 turtle.setup(400,500)
 wn = turtle.Screen()
-wn.title("Using a timer")
+wn.title("Traffic Light")
 wn.bgcolor("lightgreen")
-
 tess = turtle.Turtle()
-tess.color("purple")
-tess.pensize(3)
 
-def h1():
-    tess.forward(100)
-    tess.left(56)
-    wn.ontimer(h1, 600)
 
-h1()
+def draw_housing():
+    tess.pensize(3)
+    tess.color("black", "darkgrey")
+    tess.begin_fill()
+    tess.forward(80)
+    tess.left(90)
+    tess.forward(200)
+    tess.circle(40, 180)
+    tess.forward(200)
+    tess.left(90)
+    tess.end_fill()
+
+draw_housing()
+
+tess.penup()
+tess.forward(40)
+tess.left(90)
+tess.forward(50)
+
+tess.shape("circle")
+tess.shapesize(3)
+tess.fillcolor("green")
+
+state_num = 0
+
+def advance_state_machine():
+    global state_num
+    if state_num == 0:
+        tess.forward(70)
+        tess.fillcolor('orange')
+        state_num = 1
+    elif state_num == 1:
+        tess.forward(70)
+        tess.fillcolor('red')
+        state_num = 2
+    else:
+        tess.back(140)
+        tess.fillcolor('green')
+        state_num = 0
+
+wn.onkey(advance_state_machine, "space")
+
+wn.listen()
 wn.mainloop()
 
