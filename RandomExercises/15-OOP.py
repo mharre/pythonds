2ad2e0def082afd203edb2015d135fd6ea1f4765
unknown <matthewharre@gmail.com>
2021-10-22 06:36:35 -0700

    added some OOP examples and practice

--- RandomExercises/15-OOP.py
+++ RandomExercises/15-OOP.py
@@ -26,12 +26,44 @@ class Point:
         q = (p.y - self.y) / (p.x - self.x)
         return q
 
+    def get_line_to(self, target):
+        slope = (target.y - self.y) // (target.x - self.x)
+        y_int = self.y - (slope * self.x)
+        final = (slope,y_int)
+        return final
+
+    def midpoint_of_circle_four_coords(self, target1, target2, target3):
+        slope_of_first_pair = (target1.y - self.y) // (target1.x - self.x)
+        slope_of_second_pair = (target3.y - target2.y) // (target3.x - target2.x)
+        
+        mx = (self.x + target1.x)/2
+        my = (self.y + target1.y)/2
+        midpoint_first_pair = Point(mx,my)
+        perpendicular_of_first_slope = slope_of_first_pair * -1
+        # y_int_of_perpendicular = self.y - (perpendicular_of_first_slope * self.x)
+        y_int_of_perpendicular = midpoint_first_pair.y - (perpendicular_of_first_slope * midpoint_first_pair.x)
+        final = (perpendicular_of_first_slope,y_int_of_perpendicular) 
+
+        mx1 = (target2.x + target3.x)/2
+        my1 = (target2.y + target3.y)/2
+        midpoint_second_pair = Point(mx1,my1)
+        perpendicular_of_second_slope = slope_of_second_pair * -1
+        # y_int_of_perpendicular1 = target2.y - (perpendicular_of_second_slope * target2.x)
+        y_int_of_perpendicular1 = midpoint_second_pair.y - (perpendicular_of_second_slope * midpoint_second_pair.x)
+        final1 = (perpendicular_of_second_slope, y_int_of_perpendicular1)
+
+        final3 = ((perpendicular_of_first_slope * perpendicular_of_second_slope), y_int_of_perpendicular)
+        ################### work in progress ##########################################
+        # return final3
+
     def __str__(self):
         return f'{self.x}, {self.y}'
 
 
-p = Point(3,4)
-q = Point(5,12)
-r = p.reflect_x()
-print(Point(4, 10).slope_from_origin())
+p = Point(-7,7)
+q = Point(1,9)
+r = Point(3,1)
+s = Point(-7,1)
+
+print(p.midpoint_of_circle_four_coords(q,r,s))
 
