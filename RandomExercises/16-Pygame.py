53f4241a314eea2a6fda495f0073afe9a8d0a3bd
unknown <matthewharre@gmail.com>
2021-10-23 05:42:19 -0700

    added small game example

---
+++ RandomExercises/16-Pygame.py
@@ -0,0 +1,143 @@
+import pygame
+import time
+
+############################################################# EXAMPLE 1 #######################################################
+# def main():
+#     """ Set up the game and run the main game loop """
+#     pygame.init() # Prepare the pygame module for use
+#     surface_sz = 480 # Desired physical surface size, in pixels.
+
+#     # Create surface of (width, height), and its window.
+#     main_surface = pygame.display.set_mode((surface_sz, surface_sz))
+
+#     # Set up some data to describe a small rectangle and its color
+#     small_rect = (300, 200, 150, 90) #x,y,width,height
+#     some_color = (255, 0, 0) # A color is a mix of (Red, Green, Blue)
+
+#     while True:
+#         ev = pygame.event.poll() # Look for any event
+#         if ev.type == pygame.QUIT: # Window close button clicked
+#             break # ... leave game loop
+        
+#         # Update your game objects and data structures here...
+        
+#         # We draw everything from scratch on each frame.
+#         # So first fill everything with the background color
+#         main_surface.fill((0, 200, 255))
+        
+#         # Overpaint a smaller rectangle on the main surface
+#         main_surface.fill(some_color, small_rect)
+        
+#         # Now the surface is ready, tell pygame to display it!
+#         pygame.display.flip()
+
+#     pygame.quit()
+
+# main()
+
+
+############################################################# EXAMPLE 2 #######################################################
+# def main():
+
+#     pygame.init() # Prepare the PyGame module for use
+#     main_surface = pygame.display.set_mode((480, 240))
+
+#     # Load an image to draw. 
+#     # PyGame handles gif, jpg, png, etc. image types.
+#     ball = pygame.image.load("ball.png")
+
+#     # Create a font for rendering text
+#     my_font = pygame.font.SysFont("Courier", 16)
+
+#     frame_count = 0
+#     frame_rate = 0
+#     t0 = time.process_time()
+
+#     while True:
+        
+#         # Look for an event from keyboard, mouse, joystick, etc.
+#         ev = pygame.event.poll()
+#         if ev.type == pygame.QUIT: # Window close button clicked?
+#             break # Leave game loop
+        
+#         # Do other bits of logic for the game here
+#         frame_count += 1
+#         if frame_count % 500 == 0:
+#             t1 = time.process_time()
+#             frame_rate = 500 / (t1-t0)
+#             t0 = t1
+
+#         # Completely redraw the surface, starting with background
+#         main_surface.fill((0, 200, 255))
+        
+#         # Put a red rectangle somewhere on the surface
+#         main_surface.fill((255,0,0), (300, 100, 150, 90))
+        
+#         # Copy our image to the surface, at this (x,y) posn
+#         main_surface.blit(ball, (100, 120))
+        
+#         # Make a new surface with an image of the text
+#         the_text = my_font.render("Frame = {0}, rate = {1:.2f} fps".format(frame_count, frame_rate), True, (0,0,0))
+#         # Copy the text surface to the main surface
+#         main_surface.blit(the_text, (10, 10))
+        
+#         # Now that everything is drawn, put it on display!
+#         pygame.display.flip()
+
+#     pygame.quit()
+
+
+# main()
+
+############################################################# EXAMPLE 3 - Queens #######################################################
+def draw_board(the_board):
+    """ Draw a chess board with queens, as determined by the the_board. """
+
+    pygame.init()
+    colors = [(255,0,0), (0,0,0)] # Set up colors [red, black]
+
+    n = len(the_board) # This is an NxN chess board.
+    surface_sz = 480 # Proposed physical surface siz
+    sq_sz = surface_sz // n # sq_sz is length of a square.
+    surface_sz = n * sq_sz # Adjust to exactly fit n squares.
+    
+    # Create the surface of (width, height), and its window.
+    surface = pygame.display.set_mode((surface_sz, surface_sz))
+    
+    ball = pygame.image.load("ball.png")
+    
+    # Use an extra offset to centre the ball in its square.
+    # If the square is too small, offset becomes negative,
+    # but it will still be centered :
+    ball_offset = (sq_sz-ball.get_width()) // 2
+
+    while True:
+        # Look for an event from keyboard, mouse, etc.
+        ev = pygame.event.poll()
+        if ev.type == pygame.QUIT:
+            break;
+
+        # Draw a fresh background (a blank chess board)
+        for row in range(n): # Draw each row of the board.
+            c_indx = row % 2 # Alternate starting color
+            for col in range(n): # Run through cols drawing squares
+                the_square = (col*sq_sz, row*sq_sz, sq_sz, sq_sz)
+                surface.fill(colors[c_indx], the_square)
+                # Now flip the color index for the next square
+                c_indx = (c_indx + 1) % 2
+
+        # Now that squares are drawn, draw the queens.
+        for (col, row) in enumerate(the_board):
+            surface.blit(ball,
+            (col*sq_sz+ball_offset,row*sq_sz+ball_offset))
+            
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    draw_board([0, 5, 3, 1, 6, 4, 2]) # 7 x 7 to test window size
+    draw_board([6, 4, 2, 0, 5, 7, 1, 3])
+    draw_board([9, 6, 0, 3, 10, 7, 2, 4, 12, 8, 11, 5, 1]) # 13 x 13
+    draw_board([11, 4, 8, 12, 2, 7, 3, 15, 0, 14, 10, 6, 13, 1, 5, 9])
+
